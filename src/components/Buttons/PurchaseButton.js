f7db13e2b125f2d84b1dc20094400b2ce926472d
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-12-27 16:13:12 +0530

    Added transitions, transforms to purchase button

--- src/components/Buttons/PurchaseButton.js
+++ src/components/Buttons/PurchaseButton.js
@@ -2,16 +2,24 @@ import * as React from "react"
 import styled from "styled-components"
 import { Caption2, SmallText } from "../Styles/TextStyles"
 import { Link } from "gatsby"
+import creditBtn from "../../images/icons/credit.svg"
+import ringIcon from "../../images/icons/icon-ring.svg"
 
 export default function PurchaseButton(props) {
   const { title, subtitle } = props
 
   return (
-    <Link to='/page-2'>
-    <Wrapper>
-      <Title>{title || "Get Pro Access"}</Title>
-      <Subtitle>{subtitle || "$19 per month"}</Subtitle>
-    </Wrapper>
+    <Link to="/page-2">
+      <Wrapper>
+        <IconWrapper>
+          <Icon src={creditBtn} />
+          <Ring src={ringIcon} />
+        </IconWrapper>
+        <TextWrapper>
+          <Title>{title || "Get Pro Access"}</Title>
+          <Subtitle>{subtitle || "$19 per month"}</Subtitle>
+        </TextWrapper>
+      </Wrapper>
     </Link>
   )
 }
@@ -26,6 +34,42 @@ const Wrapper = styled.div`
     inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
   border-radius: 20px;
   border: 0px;
+  display: grid;
+  grid-template-columns: 53px auto;
+  align-content: center;
+  gap: 20px;
+  transition: 1s 0.2s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  :hover {
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+      0px 30px 60px rgba(23, 0, 102, 0.5),
+      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
+      transform: translateY(-3px);
+  }
+`
+const IconWrapper = styled.div`
+  width: 45px;
+  height: 45px;
+  background: linear-gradient(200.44deg, #4316db 13.57%, #9076e7 98.38%);
+  border-radius: 50%;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  justify-self: center;
+  position: relative;
+`
+const Icon = styled.img`
+  width: 29px;
+  height: 29px;
+`
+const Ring = styled.img`
+  position: absolute;
+  top: -15px;
+  left: -16px;
+`
+const TextWrapper = styled.div`
+  display: grid;
+  gap: 4px;
 `
 const Title = styled(Caption2)`
   color: black;
