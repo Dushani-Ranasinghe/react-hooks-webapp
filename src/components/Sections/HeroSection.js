97a31b821c5faae8c7248ef2c2cc92f01f7b8cb8
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-12-28 23:40:07 +0530

    Created Wave component and added styles

--- src/components/Sections/HeroSection.js
+++ src/components/Sections/HeroSection.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import styled from "styled-components"
 import MockupAnimations from "../Animations/MockupAnimations"
+import WaveBackground from "../Backgrounds/WaveBackground"
 import PurchaseButton from "../Buttons/PurchaseButton"
 import { H1, MediumText } from "../Styles/TextStyles"
 import { themes } from "./ColorStyles"
@@ -11,6 +12,7 @@ export default function HeroSection() {
   return (
     <>
       <Wrapper>
+        <WaveBackground />
         <ContentWrapper>
           <TextWrapper>
             <Title>
@@ -25,7 +27,7 @@ export default function HeroSection() {
               subtitle="120+ hours of videos"
             />
           </TextWrapper>
-          <MockupAnimations/>
+          <MockupAnimations />
         </ContentWrapper>
       </Wrapper>
     </>
@@ -33,14 +35,13 @@ export default function HeroSection() {
 }
 
 const Wrapper = styled.div`
-  background: linear-gradient(190deg, #4316DB 13.57%, #9076E7 98.38%);
   overflow: hidden;
 `
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
-  display:grid;
+  display: grid;
   grid-template-columns: 360px auto;
 `
 const TextWrapper = styled.div`
