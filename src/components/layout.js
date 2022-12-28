a011e547824c1e0edc7face5297cb4eb0d411e54
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-12-28 11:38:00 +0530

    Created header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { GlobalStyles } from "./Styles/GlobalStyles"
+import Header from "./header"
 
 import "./layout.css"
 
@@ -18,6 +19,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyles/>
+      <Header/>
         <main>{children}</main>
     </>
   )
