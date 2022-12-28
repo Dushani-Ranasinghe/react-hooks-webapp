94bda89cffe7acd5b4ce579450e555474c6619d9
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-12-28 10:07:04 +0530

    Added styles to mockup animations

--- src/components/Animations/MockupAnimations.js
+++ src/components/Animations/MockupAnimations.js
@@ -23,6 +23,41 @@ export default function MockupAnimations() {
 
 const Wrapper = styled.div`
   position: relative;
+  perspective: 5000;
+
+  div {
+    transform: rotateY(-20deg) rotateX(20deg);
+    transform-origin: bottom left;
+  }
+  * {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  :hover div {
+    transform: rotateX(0deg) rotateY(0deg);
+
+    &.mockup1 {
+      transform: translate(-30px, -30px);
+      transition-delay: 0.1s;
+    }
+    &.mockup2 {
+      transform: translate(0, -30px);
+      transition-delay: 0.2s;
+    }
+    &.mockup3 {
+      transition-delay: 0s;
+    }
+    &.mockup4 {
+      transform: translate(-120px, 30px);
+      transition-delay: 0.1s;
+    }
+    &.mockup5 {
+      transform: translate(-90px, 30px);
+      transition-delay: 0.2s;
+    }
+    :hover {
+      filter: brightness(160%) saturate(120%);
+    }
+  }
   .mockup1 {
     position: absolute;
     width: 183px;
@@ -30,12 +65,13 @@ const Wrapper = styled.div`
     left: 0;
     top: 0px;
 
-    background: url(${mockupImg1}), radial-gradient(
-      218.51% 281.09% at 100% 100%,
-      rgba(253, 63, 51, 0.6) 0%,
-      rgba(76, 0, 200, 0.6) 45.83%,
-      rgba(76, 0, 200, 0.6) 100%
-    );
+    background: url(${mockupImg1}),
+      radial-gradient(
+        218.51% 281.09% at 100% 100%,
+        rgba(253, 63, 51, 0.6) 0%,
+        rgba(76, 0, 200, 0.6) 45.83%,
+        rgba(76, 0, 200, 0.6) 100%
+      );
     border: 0.273134px solid rgba(255, 255, 255, 0.3);
     box-sizing: border-box;
     box-shadow: 0px 16.3881px 32.7761px rgba(99, 30, 187, 0.5);
@@ -50,12 +86,13 @@ const Wrapper = styled.div`
     left: 214px;
     top: 0px;
 
-    background: url(${mockupImg2}),linear-gradient(
-      198.85deg,
-      #4316db 12.72%,
-      #9076e7 54.49%,
-      #a2eeff 100.01%
-    );
+    background: url(${mockupImg2}),
+      linear-gradient(
+        198.85deg,
+        #4316db 12.72%,
+        #9076e7 54.49%,
+        #a2eeff 100.01%
+      );
     border: 0.27304px solid rgba(255, 255, 255, 0.5);
     box-shadow: 0px 8.19119px 16.3824px rgba(0, 0, 0, 0.1),
       0px 16.3824px 32.7648px rgba(0, 0, 0, 0.15);
@@ -71,7 +108,7 @@ const Wrapper = styled.div`
     left: 37px;
     top: 60px;
 
-    background: url(${mockupImg3}),rgba(23, 12, 61, 0.5);
+    background: url(${mockupImg3}), rgba(23, 12, 61, 0.5);
     border: 0.342305px solid rgba(255, 255, 255, 0.2);
     backdrop-filter: blur(27.3844px);
     border-radius: 6.8461px;
@@ -84,7 +121,7 @@ const Wrapper = styled.div`
     left: 194px;
     top: 262px;
 
-    background: url(${mockupImg4}),rgba(39, 20, 62, 0.3);
+    background: url(${mockupImg4}), rgba(39, 20, 62, 0.3);
     border: 0.5px solid rgba(255, 255, 255, 0.2);
     box-sizing: border-box;
     backdrop-filter: blur(27.3844px);
@@ -99,7 +136,7 @@ const Wrapper = styled.div`
     left: 616px;
     top: 262px;
 
-    background: url(${mockupImg5}),rgba(39, 20, 62, 0.2);
+    background: url(${mockupImg5}), rgba(39, 20, 62, 0.2);
     border: 0.5px solid rgba(255, 255, 255, 0.2);
     box-sizing: border-box;
     backdrop-filter: blur(27.3844px);
